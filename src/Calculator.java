d6b0d047067aa6641c234b674e838b6ce8dfb81c
Joe Edwards <Joe.Edwards@softwire.com>
2018-01-19 15:28:59 +0000

    Added a choice of operations for two random numbers

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Calculator {
     public static void main (String[] args) {
         System.out.println("Welcome to the calculator!");
+
+        // Generate two random numbers
+        Random random = new Random();
+        int first = random.nextInt(100);
+        int second = random.nextInt(100);
+
+        System.out.println("First number: " + first);
+        System.out.println("Second number: " + second);
+
+        // Choose an operation
+        System.out.println("Choose an operation (+,-,*):");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        // Perform the calculation
+        int result;
+        switch (input) {
+            case "+":
+                result = first + second;
+                break;
+            case "-":
+                result = first - second;
+                break;
+            case "*":
+                result = first * second;
+                break;
+            default:
+                throw new IllegalArgumentException("You must enter one of (+,-,*)");
+        }
+
+        // Print the result
+        System.out.println("Result: " + result);
     }
 }
